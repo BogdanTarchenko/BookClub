2999c3ba8f6d84071e1221a81d9da027f703438a
BogdanTarchenko <tarchenko.b>
2025-03-14 15:53:54 +0700

    Initial commit

---
+++ BookClub/BookClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookClubApp.swift
+//  BookClub
+//
+//  Created by Богдан Тарченко on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
