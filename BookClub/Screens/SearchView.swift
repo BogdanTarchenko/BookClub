ee85c68afab9936df46d7d90a9ae5c636c02a82c
BogdanTarchenko <tarchenko.b>
2025-03-16 23:05:41 +0700

    Добавил search bar на экран Search

--- BookClub/Screens/SearchView.swift
+++ BookClub/Screens/SearchView.swift
@@ -8,40 +8,47 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("НЕДАВНИЕ ЗАПРОСЫ")
-                    .textStyle(.h2)
-                    .foregroundStyle(.accentDark)
-                
-                Spacer(minLength: 16)
-                
-                RequestGrid()
-                
-                Spacer(minLength: 24)
-                
-                Text("ЖАНРЫ")
-                    .textStyle(.h2)
-                    .foregroundStyle(.accentDark)
-                
-                Spacer(minLength: 16)
-                
-                GenreGrid()
-                
-                Spacer(minLength: 24)
-                
-                Text("АВТОРЫ")
-                    .textStyle(.h2)
-                    .foregroundStyle(.accentDark)
-                
-                Spacer(minLength: 16)
-                
-                AuthorGrid()
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("НЕДАВНИЕ ЗАПРОСЫ")
+                        .textStyle(.h2)
+                        .foregroundStyle(.accentDark)
+                    
+                    Spacer(minLength: 16)
+                    
+                    RequestGrid()
+                    
+                    Spacer(minLength: 24)
+                    
+                    Text("ЖАНРЫ")
+                        .textStyle(.h2)
+                        .foregroundStyle(.accentDark)
+                    
+                    Spacer(minLength: 16)
+                    
+                    GenreGrid()
+                    
+                    Spacer(minLength: 24)
+                    
+                    Text("АВТОРЫ")
+                        .textStyle(.h2)
+                        .foregroundStyle(.accentDark)
+                    
+                    Spacer(minLength: 16)
+                    
+                    AuthorGrid()
+                }
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .topLeading)
             }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(Color.background)
+            .navigationTitle("")
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Поиск по книгам")
+            .toolbarBackground(Color.background, for: .navigationBar)
         }
-        .background(Color.background)
     }
 }
