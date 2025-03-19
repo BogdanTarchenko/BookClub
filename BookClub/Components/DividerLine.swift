ce23a5aad211e66338a9d7a23e6c2e838372dab0
BogdanTarchenko <tarchenko.b>
2025-03-20 03:12:07 +0700

    Провел рефакторинг DividerLine

--- BookClub/Components/DividerLine.swift
+++ BookClub/Components/DividerLine.swift
@@ -10,8 +10,16 @@ import SwiftUI
 struct DividerLine: View {
     var body: some View {
         Rectangle()
-            .frame(height: 0.33)
+            .frame(height: Metrics.height)
             .foregroundColor(.accentMedium)
-            .padding(.leading, 16)
+            .padding(.leading, Metrics.leadingPadding)
+    }
+}
+
+// MARK: - Metrics
+private extension DividerLine {
+    enum Metrics {
+        static let leadingPadding: CGFloat = 16
+        static let height: CGFloat = 0.33
     }
 }
